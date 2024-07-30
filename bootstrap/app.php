7073d9b9c17a0722821b9141c82eeaa4ec4ff307
Julian <julian@eskalink.com>
2024-07-30 14:40:24 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,24 +12,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->priority([
-            \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+        $middleware->group('web', [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class,
-            \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests::class,
-            \Illuminate\Auth\Middleware\Authorize::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
         ]);
         
         $middleware->group('api', [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            // 'throttle:api',
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
     })
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Julian;
 
 Route::get('/sports', [App\Http\Controllers\SportController::class, 'index']);
 Route::get('/log-greetings', [App\Http\Controllers\SportController::class, 'logGreetings']);
 
 Route::get('/julian', function() {
-    return Julian::capitalize('julian pramana putra'); // result => My Name Is Name
+    return App\Julian::capitalize('julian pramana putra'); // result => My Name Is Name
 });
 
 Auth::routes();
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
